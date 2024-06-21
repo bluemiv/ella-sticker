fe48659e8a326741301225fff34f7eb529ba16a4
taehongkim <public.bluemiv@gmail.com>
2024-06-21 09:53:31 +0900

    feat: Month Calendar 개발

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,12 +4,17 @@ import { MonthButton } from '../components';
 const HomePage = () => {
   return (
     <div className="">
-      <div className="m-auto max-w-[1200px] grid grid-cols-3 gap-lg">
-        {Array.from({ length: 12 }, (_, idx) => idx + 1).map((month) => (
-          <div key={month} className="flex items-center justify-center">
-            <MonthButton month={month} />
-          </div>
-        ))}
+      <div className="m-auto max-w-[1200px] flex flex-col">
+        <div className="flex items-center justify-center w-full h-[230px] text-3xl font-bold">
+          <span className="text-brand-500">엘라</span>의 지각 스티커판
+        </div>
+        <div className="grid grid-cols-3 gap-lg">
+          {Array.from({ length: 12 }, (_, idx) => idx + 1).map((month) => (
+            <div key={month} className="flex items-center justify-center">
+              <MonthButton month={month} />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
