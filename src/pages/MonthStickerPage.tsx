cb52861e5046c7708c4a681130de29b9944e3613
taehongkim <public.bluemiv@gmail.com>
2024-06-21 12:18:57 +0900

    feat: 버그 수정
    feat: 버그 수정

--- src/pages/MonthStickerPage.tsx
+++ src/pages/MonthStickerPage.tsx
@@ -13,10 +13,15 @@ const MonthStickerPage = () => {
   const params = useParams();
   const month = params?.month;
 
-  const daysInMonth = dayjs(month).daysInMonth();
+  const daysInMonth = dayjs()
+    .month(Number(month || dayjs().month()) - 1)
+    .daysInMonth();
   const workingDaysInMonth = Array.from({ length: daysInMonth }, (_, idx) => idx + 1).filter(
     (date) => {
-      const day = dayjs().date(date).day();
+      const day = dayjs()
+        .month(Number(month || dayjs().month()) - 1)
+        .date(date)
+        .day();
       return day !== 0 && day !== 6;
     },
   );
@@ -46,7 +51,7 @@ const MonthStickerPage = () => {
         <div className="absolute top-0 left-0 w-full">
           <img className="w-full h-[240px] object-cover" src={`/r/i/month${month}.webp`} alt="bg" />
         </div>
-        <div className="absolute top-0 bg-black/20 backdrop-blur-sm w-full h-full" />
+        <div className="absolute top-0 bg-black/50 backdrop-blur-sm w-full h-full" />
         <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center gap-lg text-center">
           <div className="text-2xl font-bold drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
             {month}월 스티커 판
@@ -58,7 +63,9 @@ const MonthStickerPage = () => {
       </div>
       <div className="mx-auto grid grid-cols-5 gap-xl">
         {workingDaysInMonth.map((date) => {
-          const curDate = dayjs().date(date);
+          const curDate = dayjs()
+            .month(Number(month || dayjs().month()) - 1)
+            .date(date);
           const isDiscard = stickerInfo?.discard.includes(curDate.date());
           const stickerCount = (stickerInfo?.sticker || {})[curDate.date().toString()] || 0;
           const hasSticker = stickerCount > 0;
@@ -71,7 +78,7 @@ const MonthStickerPage = () => {
                     'rounded-full flex flex-col gap-sm items-center w-[100px] h-[100px]',
                   )}
                 >
-                  <span className="">{stickerCount}개 획득</span>
+                  <span className="">{stickerCount}개</span>
                   <img
                     className="max-h-[60px] h-full animate-bounce"
                     src={`/r/i/stamp${(date % 5) + 1}.png`}
